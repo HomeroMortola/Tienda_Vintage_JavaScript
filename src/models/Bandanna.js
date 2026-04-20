ce7ebc2e59b3578e4989759690924d4cc3fe58b6
Bautista Soto <bautistasoto918@gmail.com>
2026-04-20 18:24:11 -0300

    Add Bandanna and BandannaBuilder classes

---
+++ src/models/Bandanna.js
@@ -0,0 +1,14 @@
+class Bandanna {        
+   /**
+   * @param {BandannaBuilder} builder 
+   */
+
+    constructor(builder) {
+        this.id = builder.id;
+        this.size = builder.size;
+        this.color = builder.color;
+        this.price = builder.price;
+        this.stock = builder.stock;
+        Object.freeze(this);
+    }
+}
---
+++ src/models/BandannaBuilder.js
@@ -0,0 +1,60 @@
+class BandannaBuilder {
+    constructor() {
+        this.id = 0;
+        this.size = "";
+        this.color = "";
+        this.price = 0;
+        this.stock = 0;
+    }
+
+    /**
+     * @param {number} id
+     */
+
+    setId(id) {
+        this.id = id;
+        return this;
+    }
+
+
+    /**
+     * @param {string} size
+     */
+    setSize(size) {
+        this.size = size;
+        return this;
+    }
+
+    /**
+     * @param {string} color
+     */
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+
+    /**
+     * @param {number} price
+     */
+    setPrice(price) {
+        this.price = price;
+        return this;
+    }
+
+    /**
+     * @param {number} stock
+     */
+    setStock(stock) {
+        this.stock = stock;
+        return this;
+    }
+
+
+    /**
+     * @returns {Bandanna}
+     */
+    build() {
+        return new Client(this);
+    }
+}
+
