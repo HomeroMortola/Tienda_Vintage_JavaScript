fa0506484c93c1882042d8f208f1707d7534d95f
Bautista <Bautistasoto918@gmail.com>
2026-04-21 23:52:39 -0300

    Add ProductController to handle product retrieval

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,14 @@
+import { ProductRepository } from "../repositories/ProductRepository";
+
+const productRepository = new ProductRepository();
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await productRepository.getProducts();
+        res.json(products);
+    }
+    catch (error) {
+        console.error("Error al obtener productos:", error);
+        res.status(500).json({ error: "Error al obtener productos" });
+    }
+}
