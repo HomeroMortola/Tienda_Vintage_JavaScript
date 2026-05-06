fe14996cf1b70d0859ae3732fcf35a86464753b9
Bautista <Bautistasoto918@gmail.com>
2026-05-06 14:11:22 -0300

    refactor: use environment variables for Supabase client configuration

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,11 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { ENV } from '../../env.js';
 
-const supabaseUrl = "https://zjxhoedilvqnfxbonyjh.supabase.co/"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqeGhvZWRpbHZxbmZ4Ym9ueWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNTcyOTksImV4cCI6MjA5MTkzMzI5OX0.0zb1nxwrXw7I_IV2xfr0ZNi6vmz1i7DZhptrPS3dQHo" 
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error(" Error: SUPABASE_URL o SUPABASE_KEY no están definidas en el .env")
-}
-
-// Creamos el cliente oficial de conexión
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_KEY);
