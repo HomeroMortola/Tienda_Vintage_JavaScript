bbc5a7f65695f5155bc213c21c367de6a81873b3
HomeroMortola <homemortola@gmail.com>
2026-05-07 23:57:10 -0300

    supabase.js asepta env de vercel

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,4 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import { ENV } from '../../env.js';
 
-export const supabase = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_KEY);
+const supabaseUrl = process.env.SUPABASE_URL || ENV.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY || ENV.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseUrl.startsWith('https')) {
+  throw new Error("ERROR: SUPABASE_URL no definida o inválida.");
+}
+
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
