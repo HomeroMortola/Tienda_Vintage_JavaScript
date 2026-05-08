2f22af84fbbebfae89c40c499323866cbfd056c7
HomeroMortola <homemortola@gmail.com>
2026-05-08 00:24:54 -0300

    arreglo de la coneccion con supabace

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,18 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { ENV } from '../../env.js';
 
-let supabaseUrl = process.env.SUPABASE_URL;
-let supabaseKey = process.env.SUPABASE_KEY;
-
-// 2. Si no existen (estás en local), intentamos importar tu env.js
-if (!supabaseUrl || !supabaseKey) {
-    try {
-        // La importación dinámica solo ocurre si las variables de arriba fallan
-        const module = await import('../../env.js');
-        supabaseUrl = module.ENV.SUPABASE_URL;
-        supabaseKey = module.ENV.SUPABASE_KEY;
-    } catch (e) {
-        console.error("No se encontraron variables de entorno ni archivo env.js local.");
-    }
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_KEY);
