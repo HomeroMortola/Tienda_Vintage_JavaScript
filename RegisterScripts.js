f915fc10705a4bb8c0084ec9d77b982be2b44dbe
HomeroMortola <homemortola@gmail.com>
2026-05-10 21:01:34 -0300

    Remove password field from client registration process and update Supabase client initialization

--- RegisterScripts.js
+++ RegisterScripts.js
@@ -43,7 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 .setPhone(inputs.phone.value)
                 .setLocation(inputs.location.value)
                 .setEmail(email)
-                .setPassword(password)
 
                 
                 
